00555b952c5d8da5afd03fb414f15155620fb290
tehyanowalski <89855959+tehyanowalski@users.noreply.github.com>
2022-03-02 23:31:09 -0800

    Update events-feed.blade.php

--- resources/views/events-feed.blade.php
+++ resources/views/events-feed.blade.php
@@ -1,15 +1,6 @@
 <?php
 array[
-    array(
-      "title": "CSE4500 Class",
-      "start": "2022-02-23T17:30:00",
-      "end": "2022-02-23T18:45:00"
-    ),
-    array(
-      "title": "CSE4500 Class",
-      "start": "2022-02-28T17:30:00",
-      "end": "2022-02-28T18:45:00"
-    ),   
+    [{"title":"CSE4500 Class","start":"2022-02-23T17:30:00","end":"2022-02-23T18:45:00"},{"title":"CSE4500 Class","start":"2022-02-28T17:30:00","end":"2022-02-28T18:45:00"}]  
   ]
 echo json_encode($events-feed);
 ?>
