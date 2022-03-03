c31183da3b402f46c383b50dce4fc0bb49de220d
tehyanowalski <89855959+tehyanowalski@users.noreply.github.com>
2022-03-03 15:21:08 -0800

    Update fallback.blade.php

--- resources/views/fallback.blade.php
+++ resources/views/fallback.blade.php
@@ -1,13 +1,37 @@
 @extends('adminlte::page')
 
-@section('title', 'title')
+@section('title', 'fallback')
 
 @section('content_header')
     <h1>404</h1>
 @stop
 
 @section('content')
-    <p>Oops! This page is unavailable.</p>
+    <div class="content-header">
+            <div class="container-fluid">
+                    <h1>Error</h1>
+            </div>
+        </div>
+    
+    
+    <div class="content">
+        <div class="container-fluid">
+            <div class="error-page">
+<h2 class="headline text-warning"> 404</h2>
+<div class="error-content">
+<h3><i class="fas fa-exclamation-triangle text-warning"></i> Oops! Page not found.</h3>
+<p>
+ We could not find the page you were searching for.
+Meanwhile, you may <a href="/">return to the dashboard</a> or try using the search form.
+</p>
+<form class="search-form">
+<div class="input-group">
+<input type="text" name="search" class="form-control" placeholder="Search">
+<div class="input-group-append">
+<button type="submit" name="submit" class="btn btn-warning"><i class="fas fa-search"></i>
+</button>
+</div>
+</div>
 @stop
 
 @section('css')
@@ -15,5 +39,5 @@
 @stop
 
 @section('js')
-    <script> console.log('Hi!'); </script>
+    <script src="https://cse4500-laravel9.herokuapp.com//vendor/adminlte/dist/js/adminlte.min.js"></script>
 @stop
