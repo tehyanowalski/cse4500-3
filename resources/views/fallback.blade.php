5e4faf80eeb7ae17b90d143f4a75fc22e6fdda06
tehyanowalski <89855959+tehyanowalski@users.noreply.github.com>
2022-03-03 22:46:43 -0800

    Update fallback.blade.php

--- resources/views/fallback.blade.php
+++ resources/views/fallback.blade.php
@@ -23,7 +23,7 @@
 <h3><i class="fas fa-exclamation-triangle text-warning"></i> Oops! Page not found.</h3>
 <p>
  We could not find the page you were looking for.
-Meanwhile, you may <a href="/">return to dashboard</a> or try using the search form.
+Meanwhile, you may <a href="/">return to the dashboard</a> or try using the search form.
 </p>
 <form class="search-form">
 <div class="input-group">
