a6994a1c7e4d4cf9eaf9d462d85e9e6812a2b072
tehyanowalski <89855959+tehyanowalski@users.noreply.github.com>
2022-03-09 14:01:52 -0800

    Update Calendar.php

--- app/Models/Calendar.php
+++ app/Models/Calendar.php
@@ -1,1 +1,9 @@
-
+<?php
+// set the default timezone to use.
+date_default_timezone_set('UTC');
+echo date("l");
+echo date('l jS \of F Y h:i:s A');
+echo "July 1, 2000 is on a " . date("l", mktime(0, 0, 0, 7, 1, 2000));
+echo date(DATE_RFC2822);
+echo date(DATE_ATOM, mktime(0, 0, 0, 7, 1, 2000));
+?>
