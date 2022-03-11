bf3dd1c90dca0041ce5c135c5af778babf4a077f
tehyanowalski <89855959+tehyanowalski@users.noreply.github.com>
2022-03-11 15:58:52 -0800

    Update Calendar.php

--- app/Models/Calendar.php
+++ app/Models/Calendar.php
@@ -1,9 +1,12 @@
 <?php
-// set the default timezone to use.
-date_default_timezone_set('UTC');
-echo date("l");
-echo date('l jS \of F Y h:i:s A');
-echo "July 1, 2000 is on a " . date("l", mktime(0, 0, 0, 7, 1, 2000));
-echo date(DATE_RFC2822);
-echo date(DATE_ATOM, mktime(0, 0, 0, 7, 1, 2000));
-?>
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Todo extends Model
+{
+    use HasFactory;
+    protected $fillable = ['title','progress'];
+}
