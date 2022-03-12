8d6a87977c6f1d0e6d7b047607b5e992fc179d85
tehyanowalski <89855959+tehyanowalski@users.noreply.github.com>
2022-03-11 18:24:35 -0800

    Update CalendarEvents.php

--- app/Models/CalendarEvents.php
+++ app/Models/CalendarEvents.php
@@ -8,5 +8,6 @@ use Illuminate\Database\Eloquent\Model;
 class Todo extends Model
 {
     use HasFactory;
-    protected $fillable = ['title','progress'];
+    protected $table = 'calendarevents';
+    protected $fillable = ['title','call','close'];
 }
