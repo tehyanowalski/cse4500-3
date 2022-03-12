6c133e2d34e1ef04cfb66f324220100455ea49bf
tehyanowalski <89855959+tehyanowalski@users.noreply.github.com>
2022-03-11 16:42:17 -0800

    Update 2022_03_11_170924_create_calendarevents_table.php

--- database/migrations/2022_03_11_170924_create_calendarevents_table.php
+++ database/migrations/2022_03_11_170924_create_calendarevents_table.php
@@ -16,6 +16,7 @@ return new class extends Migration
         Schema::create('todos', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
+            $table->string('title');
         });
     }
 
